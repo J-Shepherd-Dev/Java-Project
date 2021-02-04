ddb5a0d74b38dbe55a07f21d6083a2e2833a1aab
Shepstar <shepstar141@gmail.com>
2021-02-04 21:13:48 +0300

    fixed main

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,11 @@ import Filereader.FileReaderUsers;
 import Filereader.FileReaderRatings;
 import Filereader.FileReaderMovies;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-     FileReaderUsers readerUsers = new readUsers()
+    public static void main(String[] args) throws IOException {
+     FileReaderUsers readerUsers = new FileReaderUsers();
+     readerUsers.readFileUser(args);
     }
 }
